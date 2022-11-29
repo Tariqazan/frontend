4e1426c27b9c219e43f45b74cb5d09a5c283f69a
Tariqazan <45086258+Tariqazan@users.noreply.github.com>
2022-11-29 15:32:44 +0600

    updates

--- js/stickycard.js
+++ js/stickycard.js
@@ -6,4 +6,14 @@ $(function () {
             $(".pricing-card").removeClass("sticky");
         }
     });
+
+
+}); $(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > $("#photos").offset().top + $(".place-details-point").height()) {
+            $(".main-nav").addClass("sticky-header visible");
+        } else {
+            $(".main-nav").removeClass("sticky-header visible");
+        }
+    });
 });
