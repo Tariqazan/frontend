f36a2c28df27c60c66847944801e5000f12afc7b
Tariqazan <45086258+Tariqazan@users.noreply.github.com>
2022-11-29 14:34:50 +0600

    update

--- js/stickycard.js
+++ js/stickycard.js
@@ -1,6 +1,6 @@
 $(function () {
     $(window).scroll(function () {
-        if ($(window).scrollTop() > $(".place-details-point").offset().top + $(".place-details-point").height() - 200 && $(window).scrollTop() <= $(".checking-date_open").offset().top + $(".checking-date_open").height() - 200) {
+        if ($(window).scrollTop() > $(".place-details-point").offset().top + $(".place-details-point").height() + 50 && $(window).scrollTop() <= $(".checking-date_open").offset().top + $(".checking-date_open").height() - 200) {
             $(".pricing-card").addClass("sticky");
         } else {
             $(".pricing-card").removeClass("sticky");
