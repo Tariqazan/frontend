6cc6621c7d52cfe575b9699f576ea237a6c66c45
Tariqazan <45086258+Tariqazan@users.noreply.github.com>
2022-11-30 11:28:29 +0600

    updates

--- js/daterangepicker.js
+++ js/daterangepicker.js
@@ -1,3 +1,18 @@
+
+$('.total-price').hide()
+$('#btn-reserve').hide()
+function getDatesInRange(startDate, endDate) {
+    const date = new Date(startDate.getTime());
+
+    const dates = [];
+
+    while (date <= endDate) {
+        dates.push(new Date(date));
+        date.setDate(date.getDate() + 1);
+    }
+
+    return dates;
+}
 $('#checking-date_close').dateRangePicker({
     inline: true,
     format: 'MM-DD-YYYY',
@@ -12,6 +27,18 @@ $('#checking-date_close').dateRangePicker({
 })
     .bind('datepicker-change', (e, data) => {
         $('#datepicker_calender-closed').val(data.value);
+        var diff = data.date2 - data.date1;
+        var days = diff / 1000 / 60 / 60 / 24;
+        $('#total_days').html(days + 1)
+        $('.total-price').show()
+        $('#btn-reserve').show()
+        $('#btn-check').hide()
+        let dates = getDatesInRange(data.date1, data.date2)
+        let html = ''
+        for (var i = 0; i < dates.length; i++) {
+            html = html + `<div class="d-flex justify-content-between"><p>` + dates[i].getDate() + `/` + dates[i].getMonth() + `/` + dates[i].getFullYear() + `</p><p>$370</p></div>`
+        }
+        $("#dates").html(html)
     })
 
 
@@ -29,4 +56,10 @@ $('#checking-date_open').dateRangePicker({
 })
     .bind('datepicker-change', (e, data) => {
         $('#datepicker_calender-open_input').val(data.value);
+        var diff = data.date2 - data.date1;
+        var days = diff / 1000 / 60 / 60 / 24;
+        $('#total_days').html(days + 1)
+        $('.total-price').show()
+        $('#btn-reserve').show()
+        $('#btn-check').hide()
     })
